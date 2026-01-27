415930e77af456c5e62ef5d07f150f9314ec5f3f
Elham <86723263+elham-masoumi@users.noreply.github.com>
2026-01-27 21:23:09 +0000

    Update 05_load_fact_snapshot.sql

--- SQL/05_load_fact_snapshot.sql
+++ SQL/05_load_fact_snapshot.sql
@@ -1,1 +1,31 @@
+USE HR_Project;
+GO
+
+DECLARE @d DATE = CAST(GETDATE() AS DATE);
+DECLARE @DateKey INT = (YEAR(@d) * 10000) + (MONTH(@d) * 100) + DAY(@d);
+
+-- Replace today's snapshot (prevents PK duplicates)
+DELETE FROM dw.FactEmployeeSnapshot
+WHERE SnapshotDateKey = @DateKey;
+
+INSERT INTO dw.FactEmployeeSnapshot (
+  SnapshotDateKey, EmployeeKey, DepartmentKey, JobRoleKey, EducationKey,
+  MonthlyIncome, YearsAtCompany, PerformanceRating, AttritionFlag
+)
+SELECT
+  @DateKey,
+  de.EmployeeKey,
+  dd.DepartmentKey,
+  dj.JobRoleKey,
+  edu.EducationKey,
+  s.MonthlyIncome,
+  s.YearsAtCompany,
+  s.PerformanceRating,
+  CASE WHEN UPPER(LTRIM(RTRIM(s.Attrition))) = 'YES' THEN 1 ELSE 0 END
+FROM dbo.stg_hr_employee_raw s
+JOIN dw.DimEmployee de   ON de.EmployeeNumber = s.EmployeeNumber
+JOIN dw.DimDepartment dd ON dd.DepartmentName = s.Department
+JOIN dw.DimJobRole dj    ON dj.JobRoleName = s.JobRole
+JOIN dw.DimEducation edu ON edu.EducationLevel = s.Education;
+GO
 
