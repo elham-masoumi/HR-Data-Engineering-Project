121b01e361dfb94a8bdb09e76446f7cca18705ca
Elham <86723263+elham-masoumi@users.noreply.github.com>
2026-01-27 21:22:49 +0000

    Update 04_dimdate_snapshot.sql

--- SQL/04_dimdate_snapshot.sql
+++ SQL/04_dimdate_snapshot.sql
@@ -1,1 +1,13 @@
+USE HR_Project;
+GO
+
+DECLARE @d DATE = CAST(GETDATE() AS DATE);
+DECLARE @DateKey INT = (YEAR(@d) * 10000) + (MONTH(@d) * 100) + DAY(@d);
+
+IF NOT EXISTS (SELECT 1 FROM dw.DimDate WHERE DateKey = @DateKey)
+BEGIN
+  INSERT INTO dw.DimDate (DateKey, FullDate, [Year], [Month], MonthName)
+  VALUES (@DateKey, @d, YEAR(@d), MONTH(@d), DATENAME(MONTH, @d));
+END
+GO
 
