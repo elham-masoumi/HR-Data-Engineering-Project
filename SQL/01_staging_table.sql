86f9a91ae1c701f3563002fe47b54c49547449d6
Elham <86723263+elham-masoumi@users.noreply.github.com>
2026-01-27 21:21:40 +0000

    Update 01_staging_table.sql

--- SQL/01_staging_table.sql
+++ SQL/01_staging_table.sql
@@ -1,1 +1,27 @@
+USE HR_Project;
+GO
+
+IF OBJECT_ID('dbo.stg_hr_employee_raw','U') IS NOT NULL
+    DROP TABLE dbo.stg_hr_employee_raw;
+GO
+
+CREATE TABLE dbo.stg_hr_employee_raw (
+    -- فقط ستون‌های کلیدی که در DW استفاده می‌کنی را نگه دار
+    EmployeeNumber INT NULL,
+    Age INT NULL,
+    Gender NVARCHAR(20) NULL,
+    MaritalStatus NVARCHAR(20) NULL,
+    Department NVARCHAR(50) NULL,
+    JobRole NVARCHAR(100) NULL,
+    Education INT NULL,
+    EducationField NVARCHAR(50) NULL,
+    BusinessTravel NVARCHAR(50) NULL,
+    Over18 NVARCHAR(10) NULL,
+    OverTime NVARCHAR(5) NULL,
+    MonthlyIncome INT NULL,
+    YearsAtCompany INT NULL,
+    PerformanceRating INT NULL,
+    Attrition NVARCHAR(5) NULL
+);
+GO
 
